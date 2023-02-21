b8d1023aa268dbc1ff21944339b9a6d5988d21de
ravevy <beyzbaser@gmail.com>
2023-02-21 03:57:03 +0300

    release

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { useEffect, useState } from "react";
 
-function App() {
-  return <div className="navbar"></div>;
+function App(): JSX.Element {
+  const [loaded, setLoaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
+  return { loaded } && <Navbar />;
 }
 
 export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,120 @@
+import { useState, useEffect } from "react";
+
+export default function Navbar(): JSX.Element {
+  const [serviceExpand, setServiceExpand] = useState<boolean>(false);
+  const [aboutExpand, setAboutExpand] = useState<boolean>(false);
+
+  function expandingServiceDropdown(input: boolean): void {
+    setServiceExpand(input);
+    setAboutExpand(false);
+  }
+  function expandingAboutDropdown(input: boolean): void {
+    setAboutExpand(input);
+    setServiceExpand(false);
+  }
+  function closeAllDropdowns(): void {
+    setAboutExpand(false);
+    setServiceExpand(false);
+  }
+
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      e.preventDefault();
+      let target = e.target as HTMLElement;
+
+      if (target.className.includes("dropdown")) {
+        if (target.className.includes("services")) {
+          expandingServiceDropdown(!serviceExpand);
+        } else if (target.className.includes("about")) {
+          expandingAboutDropdown(!aboutExpand);
+        }
+      } else {
+        closeAllDropdowns();
+      }
+    };
+
+    document.addEventListener("click", handleClick);
+
+    const handleKeyup = (e: KeyboardEvent) => {
+      const key = e.key;
+      const target = e.target as HTMLElement;
+
+      if (key === "Tab") {
+        if (!target.className.includes("services") && serviceExpand) {
+          setServiceExpand(false);
+        }
+        if (!target.className.includes("about") && aboutExpand) {
+          setAboutExpand(false);
+        }
+      }
+      if (key === "Enter") {
+        if (target.className.includes("services")) {
+          expandingServiceDropdown(true);
+        }
+        if (target.className.includes("about")) {
+          expandingAboutDropdown(true);
+        }
+      }
+      if (key === "Escape") {
+        closeAllDropdowns();
+      }
+    };
+    document.addEventListener("keyup", handleKeyup);
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+      document.removeEventListener("keyup", handleKeyup);
+    };
+  }, [aboutExpand, serviceExpand]);
+
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#services" className="dropdown services">
+            Services
+          </a>
+          <ul
+            id="services"
+            aria-expanded={serviceExpand}
+            aria-hidden={serviceExpand}
+          >
+            <li>
+              <a className="services" href="#service-one">
+                Service 1
+              </a>
+            </li>
+            <li>
+              <a className="services" href="#service-two">
+                Service 2
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="#about" className="dropdown about">
+            About
+          </a>
+          <ul aria-expanded={aboutExpand} aria-hidden={aboutExpand}>
+            <li>
+              <a className="about" href="#history">
+                History
+              </a>
+            </li>
+            <li>
+              <a className="about" href="#members">
+                Members
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
